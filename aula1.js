bef6f4199d99215c859b1b8737cd4225311e4e94
Gustavo.Rodrigues <rodrigues.gustavo20@escola.pr.gov.br>
2026-03-09 08:59:26 -0300

    Add files via upload

---
+++ aula1.js
@@ -0,0 +1,11 @@
+// comentário
+// const -> variável constante (não pode ser alterada)
+// let -> variável local
+// var -> variável global
+let nome = "Gustavo"
+let altura = 1.74
+let idade = 15
+let cabelo = "preto"
+let moletom = "cinza"
+console.log("Olá, meu nome é " + nome + "! Possuo " + idade + " anos, minha cor de cabelo é " + cabelo + " estou usando moletom " + moletom)
+
